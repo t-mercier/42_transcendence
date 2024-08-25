e9127f38cd7ae0b904a581d07cbf6a46b0a84728
alfa <alfa@devo.pw>
2024-08-25 14:25:38 +0200

    some fixes

--- client/src/components/InviteReceiver.tsx
+++ client/src/components/InviteReceiver.tsx
@@ -46,7 +46,7 @@ const InviteReceiver = () => {
     left: '50%',
     transform: 'translate(-50%, -50%)',
     width: 400,
-    bgcolor: 'background.paper',
+    bgcolor: 'palette.background',
     border: '2px solid #000',
     boxShadow: 24,
     p: 4,
